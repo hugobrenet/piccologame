aae3068d10150ea9b2b85d653c5dcbaedf0b5184
brenet <hugo.bnenet@etu.u-pec.fr>
2023-02-15 16:42:20 +0100

    Add question and shuffle it

--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 def player():
     print("Bienvenue, qui veux jouer ? ")
     listofplayer = []
-
     for i in range(10):
         name = input("Ajouter les noms des joueurs(max 10 et stop pour arreter) : ")
         if name == "stop":
@@ -9,6 +8,31 @@ def player():
         listofplayer.append(name)
     return listofplayer
 
+
+def question():
+    question = [
+        "Comment ça va ?",
+        "La capital de la France ?",
+        "Qu'elle est ton prénom ?",
+        "Quel age tu as ?",
+        "Tu es à la fac ?",
+        "Tu utilises un ordinateur ?"
+    ]
+    return question
+
+
 def game():
+    import random
     nameofplayer = player()
-    print(nameofplayer)
+    random.shuffle(nameofplayer)
+    questionforplayer = question()
+    random.shuffle(questionforplayer)
+
+    dict = {}
+    for name in nameofplayer:
+        for request in questionforplayer:
+            dict[name] = request
+            questionforplayer.remove(request)
+            break
+    print(dict)
+
